96d2c8f413b9478770884539ea750f7355d30b8b
alfaraday <arianaloreleifaraday@gmail.com>
2017-02-02 21:13:26 -0500

    render HTML

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var score = 0
+var qNum = 0
 
 var question template = {question: '',
 choices: [],
@@ -37,3 +38,32 @@ choices: ['a Crysal Gem', 'a fusion', 'stronger than you', 'All of the above'],
 answer: 'All of the above',
 class: 'garnet'
 }]
+
+function renderHTML(content, x){
+	return '<label class="q-num'+content[x].class+'">Question x+1 of 5</label>'+
+		'<p class="question">'+content[x].question+'</p>'+
+		'<div class="answers">'+
+			'<input type="radio" name="q" id="a" value="0"><label for="a">'+content[x].choices[0]+'</label><br>'+
+			'<input type="radio" name="q" id="b" value="1"><label for="b">'+content[x].choices[1]+'</label><br>'+
+			'<input type="radio" name="q" id="c" value="2"><label for="c">'+content[x].choices[2]+'</label><br>'+
+			'<input type="radio" name="q" id="d" value="3"><label for="d">'+content[x].choices[3]+'</label><br>'+
+		'</div>'+
+		'<p class="correct feedback ">Correct!</p>'+
+		'<p class="incorrect feedback ">Wrong! The correct answer is '+content[x].answer+'</p>'+
+		'<button type="button" class="next">Next</button>'
+};
+
+//Display first question
+$('.title-page').on('click', 'button.start', function(event){
+	event.preventDefault();
+	$('.title-page').addClass('hidden');
+
+	$('.quiz-page').removeClass('hidden');
+});
+
+
+
+
+
+
+
