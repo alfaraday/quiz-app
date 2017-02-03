b0788663e9c5956ae04dd73476215799eca5eef9
alfaraday <arianaloreleifaraday@gmail.com>
2017-02-02 20:35:28 -0500

    Added questions

---
+++ app.js
@@ -0,0 +1,39 @@
+var score = 0
+
+var question template = {question: '',
+choices: [],
+answer: '',
+class: ''
+}
+
+var content = [
+{question: 'Before Steven freed her, Lapis Lazuli was trapped for thousands of years in a:',
+choices: ['Tower', 'Book', 'Mirror', 'Cave'],
+answer: 'Mirror',
+class: 'lapis-lazuli'
+},
+{question: 'What is Smoky Quartz’s weapon?',
+choices: ['A yo-yo', 'A cannon', 'A war hammer', 'A sling-shot'],
+answer: 'A yo-yo',
+class: 'smoky-quartz'
+},
+{question: 'What do Peridot and Lapis refer to pieces of art as?',
+choices: ['Shiny arcs', 'Clods', 'Gleep glops', 'Meep morps'],
+answer: 'Meep morps',
+class: 'peridot'
+},
+{question: 'What is Greg’s family name?',
+choices: ['Maheswaran', 'DeMayo', 'Dewey', 'Miller'],
+answer: 'DeMayo',
+class: 'greg'
+},
+{question: 'What is the first type of sentient plant that Steven grows?',
+choices: ['Bushes', 'Pumpkins', 'Watermelons', 'Daisies'],
+answer: 'Watermelons',
+class: 'steven'
+},
+{question: 'Garnet is:',
+choices: ['a Crysal Gem', 'a fusion', 'stronger than you', 'All of the above'],
+answer: 'All of the above',
+class: 'garnet'
+}]
